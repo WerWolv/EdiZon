ac4c8c9e86a85b3bed1897f9c32d4f4863e02619
Nikolaij Sägesser <werwolv98@hotmail.com>
2018-05-17 00:49:49 +0200

    Tried to fix rendering of title icons. Still needs malloc size ajustments

--- source/gui_main.cpp
+++ source/gui_main.cpp
@@ -1,7 +1,6 @@
 #include "gui_main.hpp"
 #include "save.hpp"
 #include "title.hpp"
-
 #include <unordered_map>
 
 extern std::unordered_map<uint64_t, void*> titles;
@@ -17,8 +16,7 @@ GuiMain::~GuiMain() {
 void GuiMain::draw() {
   Gui::drawRectangle(0, 0, Gui::m_framebuffer_width, Gui::m_framebuffer_height, currTheme.backgroundColor);
 
-  Gui::drawImage(100, 100, 256, 256, ((Title*)titles[0x01007EF00011E000UL])->getTitleIcon(), IMAGE_MODE_RGB24);
-
+  Gui::drawImage(100, 100, 256, 256, ((Title*)titles.at(0x0100000000010000))->getTitleIcon(), IMAGE_MODE_RGB24);
   Gui::drawShadow(100, 50, 256, 256);
   gfxFlushBuffers();
   gfxSwapBuffers();
--- source/title.cpp
+++ source/title.cpp
@@ -3,6 +3,8 @@
 Title::Title(FsSaveDataInfo saveDataInfo) {
   //_getTitleName(saveDataInfo.titleID, this->m_titleName);
     this->m_titleID = saveDataInfo.titleID;
+    //TODO: FIX SIZE
+    this->m_titleIcon = (uint8_t*)malloc(256*256*4);
   _getTitleIcon(this->m_titleID, &this->m_titleIcon);
 }
 

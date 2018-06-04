875f58631ec88e4efa300fc4d006f0a12baa327c
WerWolv98 <werwolv98@gmail.com>
2018-06-04 18:09:31 +0200

    Fixed touch in title screen with > 10 games

--- source/gui_main.cpp
+++ source/gui_main.cpp
@@ -29,7 +29,7 @@ GuiMain::~GuiMain() {
 void GuiMain::draw() {
   Gui::beginDraw();
 
-  xOffset = m_selected.titleIndex > 5 ? m_selected.titleIndex > Title::g_titles.size() - 5 ? 256 * ceil((Title::g_titles.size() - (Title::g_titles.size() > 10 ? 11.0F : 9.0F)) / 2) : 256 * ceil((m_selected.titleIndex - 5.0F) / 2) : 0;
+  xOffset = m_selected.titleIndex > 5 ? m_selected.titleIndex > Title::g_titles.size() - 5 ? 256 * (ceil((Title::g_titles.size() - (Title::g_titles.size() > 10 ? 11.0F : 9.0F)) / 2.0F) + Title::g_titles.size() > 10 ? 1 : 0) : 256 * ceil((m_selected.titleIndex - 5.0F) / 2.0F) : 0;
   Gui::drawRectangle(0, 0, Gui::framebuffer_width, Gui::framebuffer_height, currTheme.backgroundColor);
   Gui::drawRectangle(0, 0, Gui::framebuffer_width, 10, COLOR_BLACK);
 
@@ -129,7 +129,7 @@ void GuiMain::onInput(u32 kdown) {
 void GuiMain::onTouch(touchPosition &touch) {
   switch(selectionState) {
     case TITLE_SELECT: {
-      u8 x = floor((float)touch.px / 256.0F);
+      u8 x = floor((float)(touch.px + xOffset) / 256.0F);
       u8 y = floor((float)touch.py / 256.0F);
       u8 title = y + x * 2;
 

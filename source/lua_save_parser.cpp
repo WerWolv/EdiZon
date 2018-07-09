3a3edd780b481713bbeaf4392b806936f175f577
WerWolv98 <werwolv98@gmail.com>
2018-07-09 23:42:07 +0200

    Removed unused debug code

--- source/lua_save_parser.cpp
+++ source/lua_save_parser.cpp
@@ -22,35 +22,6 @@ LuaSaveParser::~LuaSaveParser() {
   lua_close(m_luaState);
 }
 
-static void stackDump (lua_State *L) {
-      int i;
-      int top = lua_gettop(L);
-      for (i = 1; i <= top; i++) {  /* repeat for each level */
-        int t = lua_type(L, i);
-        switch (t) {
-
-          case LUA_TSTRING:  /* strings */
-            printf("`%s'", lua_tostring(L, i));
-            break;
-
-          case LUA_TBOOLEAN:  /* booleans */
-            printf(lua_toboolean(L, i) ? "true" : "false");
-            break;
-
-          case LUA_TNUMBER:  /* numbers */
-            printf("%g", lua_tonumber(L, i));
-            break;
-
-          default:  /* other values */
-            printf("%s", lua_typename(L, t));
-            break;
-
-        }
-        printf("  ");  /* put a separator */
-      }
-      printf("\n");  /* end the listing */
-    }
-
 void LuaSaveParser::luaInit(std::string filetype) {
   m_luaState = luaL_newstate();
 

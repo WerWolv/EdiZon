755c66fba82745cd2d035c4e2c0b507d6dbf4f88
thomasnet <39246639+thomasnet-mc@users.noreply.github.com>
2018-07-23 20:53:34 +0200

    Fixed updating

--- source/update_manager.cpp
+++ source/update_manager.cpp
@@ -1,19 +1,26 @@
 #include "update_manager.hpp"
 
-#include <stdio.h>
+#include <cstdio>
 
 #include <iostream>
+#include <fstream>
 #include <cstring>
 
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <cerrno>
+#include <utime.h>
 
 #include <curl/curl.h>
 
+#include "json.hpp"
+
 #include "gui.hpp"
 
-#define EDIZON_URL "http://werwolv.net/EdiZon"
+#define EDIZON_URL "http://werwolv.net"
+
+using namespace nlohmann;
 
 CURL *curl;
 
@@ -40,93 +47,127 @@ size_t writeToFile(void *ptr, size_t size, size_t nmemb, FILE *stream) {
     return written;
 }
 
-std::vector<std::pair<std::string, std::string>> parseUpdateFile(std::string content, std::string *out_versionString) {
-  std::vector<std::pair<std::string, std::string>> updatePathPair;
-  std::vector<std::string> lines = Gui::split(content, '\n');
+void deleteFile(std::string path)
+{
+  printf("Deleting %s.\n", path.c_str());
 
-  *out_versionString = lines[0];
-  lines.erase(lines.begin());
+  remove(path.c_str());
+}
 
-  for (auto line : lines) {
-    std::vector<std::string> paths = Gui::split(line, ' ');
-    updatePathPair.push_back(std::make_pair(std::string(EDIZON_URL) + paths[0], paths[1]));
+void updateFile(std::string path)
+{
+  printf("Updating %s.\n", path.c_str());
+
+  std::string url = EDIZON_URL;
+  url.append(path);
+
+  FILE* fp;
+  if (path.compare("/EdiZon/EdiZon.nro") == 0)
+  {
+    deleteFile("/switch/EdiZon.nro");
+    mkdir("/switch/EdiZon", 0777);
+    path = "/switch/EdiZon/EdiZon.nro";
   }
+  fp = fopen(path.c_str(), "wb");
 
-  return updatePathPair;
+  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeToFile);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
+  CURLcode res = curl_easy_perform(curl);
+
+  if (res != CURLE_OK)
+    printf("2nd CURL perform failed: %s\n", curl_easy_strerror(res));
+
+  fclose(fp);
 }
 
-void trim(std::string& s) {
-    while(s.compare(0,1," ")==0)
-        s.erase(s.begin());
-    while(s.size()>0 && s.compare(s.size()-1,1," ")==0)
-        s.erase(s.end()-1);
-}
+bool UpdateManager::checkUpdate()
+{
+  if (!curl)
+    return false;
 
-bool UpdateManager::checkUpdate() {
   CURLcode res;
-  FILE *fp;
   std::string str;
 
-  curl_easy_setopt(curl, CURLOPT_URL, EDIZON_URL"/dir.php");
+  curl_easy_setopt(curl, CURLOPT_URL, EDIZON_URL "/EdiZon/versionlist.php");
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeToString);
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, &str);
 
   res = curl_easy_perform(curl);
 
   if (res != CURLE_OK) {
-    printf("CURL perform failed: %s\n", curl_easy_strerror(res));
+    printf("1st CURL perform failed: %s\n", curl_easy_strerror(res));
     return false;
   }
 
-  m_downloadPaths = parseUpdateFile(str, &m_versionString);
+  printf("Returned data: %s\n", str.c_str());
+  if (str.compare(0, 1, "{") != 0)
+  {
+    printf("Invalid downloaded JSON!\n");
+    return false;
+  }
+
+  json remote = json::parse(str);
 
   mkdir("/EdiZon", 0777);
   mkdir("/EdiZon/editor", 0777);
   mkdir("/EdiZon/editor/scripts", 0777);
   mkdir("/EdiZon/editor/scripts/lib", 0777);
 
-  fp = fopen("/EdiZon/version", "r+");
+  bool updatedFile = false;
 
-  if (fp != nullptr) {
-    u8 fileLength;
-    char *localVersionString;
-
-    fseek(fp, 0L, SEEK_END);
-    fileLength = ftell(fp);
-    rewind(fp);
-
-    localVersionString = new char[fileLength];
-
-    fread(localVersionString, sizeof(char), fileLength, fp);
-
-    if (std::string(localVersionString) == m_versionString) {
-      fclose(fp);
+	std::ifstream i("/EdiZon/editor/update.json");
+	if (!i.is_open())
+	{
+		printf("File didn't exist, will create it.\n");
+		for (json::iterator it = remote.begin(); it != remote.end(); ++it)
+			updateFile(it.key());
+		updatedFile = true;
+	}
+	else
+	{
+		printf("File opened.\n");
+		std::string buf;
+		std::getline(i, buf);
+		if (buf.size() == 0) return false;
+		printf("Read from SD card: %s\n", buf.c_str());
+    if (buf.compare(0, 1, "{") != 0)
+    {
+      printf("Invalid JSON on SD card!\n");
+      deleteFile("/EdiZon/editor/update.json");
       return false;
     }
-  }
+		auto local = json::parse(buf);
+		i.close();
 
-  fclose(fp);
+		for (json::iterator it = remote.begin(); it != remote.end(); ++it)
+		{
+			if (local.find(it.key()) == local.end() || local[it.key()] < remote[it.key()])
+			{
+				updateFile(it.key());
+				updatedFile = true;
+			}
+		}
 
-  fp = fopen("/EdiZon/version", "w+");
-  fputs(m_versionString.c_str(), fp);
+		for (json::iterator it = local.begin(); it != local.end(); ++it)
+		{
+			if (remote.find(it.key()) == remote.end())
+				deleteFile(it.key());
+		}
+	}
 
-  fclose(fp);
+	if (updatedFile)
+	{
+		printf("Writing updated file to SD.\n");
+		std::ofstream o("/EdiZon/editor/update.json");
+		if (o.is_open()) {
+			o << str;
+			o.close();
+		}
+		else
+			printf("Error while opening the output file.\n");
+	}
 
-  for (auto path : m_downloadPaths) {
-    fp = fopen(path.second.c_str(), "wb");
-
-    curl_easy_setopt(curl, CURLOPT_URL, path.first.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeToFile);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-
-    res = curl_easy_perform(curl);
-
-    if (res != CURLE_OK)
-      printf("CURL perform failed: %s\n", curl_easy_strerror(res));
-
-    fclose(fp);
-  }
-
-  return true;
-
+  return updatedFile;
 }
